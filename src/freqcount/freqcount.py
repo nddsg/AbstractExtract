975c93a0672eb0257d4f1381c6e59a1e075b3f5d
Norman S. Morales <norman.513@gmail.com>
2015-11-09 14:36:49 -0500

    Added Corenlp substituion and reverb output

--- src/freqcount/freqcount.py
+++ src/freqcount/freqcount.py
@@ -1,37 +1,39 @@
 import csv
 import sys
 import operator
+from decimal import *
 
 csv.field_size_limit(sys.maxsize)
 
-extracts = list(csv.reader(open('1000_extract.txt', 'rb'), delimiter='\t'))
-
+extracts = list(csv.reader(open('corenlp_1000.csv.sub.extract.txt', 'rb'), delimiter='\t'))
+confidence = Decimal(0.5)
 nouns = dict()
 actions = dict()
 objects = dict()
 triples = dict()
 
 for entry in extracts:
-	if entry[len(entry)-3] in nouns:
-		nouns[entry[len(entry)-3]] += 1
-	else:
-		nouns[entry[len(entry)-3]] = 1
-		
-	if entry[len(entry)-2] in actions:
-		actions[entry[len(entry)-2]] += 1
-	else:
-		actions[entry[len(entry)-2]] = 1
-		
-	if entry[len(entry)-1] in objects:
-		objects[entry[len(entry)-1]] += 1
-	else:
-		objects[entry[len(entry)-1]] = 1
-	
-	tip = entry[len(entry)-3] + " " + entry[len(entry)-2] + " " + entry[len(entry)-1];
-	if tip in triples:
-		triples[tip] += 1
-	else:
-		triples[tip] = 1
+	if Decimal(entry[len(entry)-7]) >= Decimal(confidence):
+		if entry[len(entry)-3] in nouns:
+			nouns[entry[len(entry)-3]] += 1
+		else:
+			nouns[entry[len(entry)-3]] = 1
+
+		if entry[len(entry)-2] in actions:
+			actions[entry[len(entry)-2]] += 1
+		else:
+			actions[entry[len(entry)-2]] = 1
+
+		if entry[len(entry)-1] in objects:
+			objects[entry[len(entry)-1]] += 1
+		else:
+			objects[entry[len(entry)-1]] = 1
+
+		tip = entry[len(entry)-3] + " " + entry[len(entry)-2] + " " + entry[len(entry)-1];
+		if tip in triples:
+			triples[tip] += 1
+		else:
+			triples[tip] = 1
 		
 sortednouns = sorted(nouns.items(), key=operator.itemgetter(1))
 sortedactions = sorted(actions.items(), key=operator.itemgetter(1))
@@ -53,25 +55,25 @@ print "\nTop 10 Triple"
 for i in range(1,11):
 	print sortedtriples[len(sortedtriples)-i]
 	
-output = open('noun-freq.txt', 'w')
+output = open('noun-freq-corenlp.txt', 'w')
 output.write("Nouns Frequency \n")
 for noun in sortednouns:
 	output.write(noun[0] + "," + str(noun[1]) + "\n")
 output.close()
 
-output = open('action-freq.txt', 'w')
+output = open('action-freq-corenlp.txt', 'w')
 output.write("Actions Frequency \n")
 for actions in sortedactions:
 	output.write(actions[0] + "," + str(actions[1]) + "\n")
 output.close()
 
-output = open('object-freq.txt', 'w')
+output = open('object-freq-corenlp.txt', 'w')
 output.write("Objects Frequency \n")
 for objects in sortedobjects:
 	output.write(objects[0] + "," + str(objects[1]) + "\n")
 output.close()
 
-output = open('triple-freq.txt', 'w')
+output = open('triple-freq-corenlp.txt', 'w')
 output.write("Triples Frequency \n")
 for triples in sortedtriples:
 	output.write(triples[0] + "," + str(triples[1]) + "\n")	
